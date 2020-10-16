78371016461c8cc669d011ac6d9a8c562f1d6717
Lalit Yadav <lyvworks@gmail.com>
2020-10-16 15:24:40 +0530

    Updates

---
+++ precache-manifest.13140d29504aeaaf17ac608b5d8a127d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54659fa10f71bca722fb8f46386b0251",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be96aea38e1e48f98462",
+    "url": "./static/css/main.7ac95cf5.chunk.css"
+  },
+  {
+    "revision": "fbddff760cbdea3c9a9a",
+    "url": "./static/js/2.9b6379ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9b6379ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be96aea38e1e48f98462",
+    "url": "./static/js/main.3a9cd40d.chunk.js"
+  },
+  {
+    "revision": "cfadd6680b1737b20804",
+    "url": "./static/js/runtime-main.93e5cba5.js"
+  }
+]);
