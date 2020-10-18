cc9a6c22b9aa791fbbf690a8ddb0b3a63a125b31
Lalit Yadav <lyvworks@gmail.com>
2020-10-18 20:26:12 +0530

    updated readme

--- src/components/row.js
+++ src/components/row.js
@@ -7,13 +7,11 @@ const StyledGrid = styled.div`
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 	width: 100%;
-	gap: ${({ theme }) =>
-		`${2 * theme.variables.gutter}rem ${theme.variables.gutter}rem`};
-	margin: 0 ${({ theme }) => -1 * theme.variables.gutter}rem;
+	gap: ${({ theme }) => `${2 * theme.variables.gutter}rem ${theme.variables.gutter}rem`};
+
 	${breakpointMax('sm')} {
-		grid-template-columns: repeat(auto-fit, minmax(175px, 1fr));
-		margin: 0;
-	}s
+		gap: ${({ theme }) => `${1 * theme.variables.gutter}rem ${0.5 * theme.variables.gutter}rem`};
+	}
 `
 
 const Row = ({ children, ...rest }) => <StyledGrid>{children}</StyledGrid>
